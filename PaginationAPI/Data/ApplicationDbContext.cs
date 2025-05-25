ae0ab1c7000ba2bcc3e67d68f3126dad051bc35b
KerolosNabil7 <kerolosnabil926@gmail.com>
2025-05-25 13:59:08 +0300

    Add ApplicationDbContext class

---
+++ PaginationAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using Microsoft.EntityFrameworkCore;
+using PaginationAPI.Models;
+
+namespace PaginationAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
